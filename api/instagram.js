4c8360332c10ce702dc0e02a74f10c8f7648605c
Rohit Khanal <rohitkhanal321@gmail.com>
2026-07-15 04:00:52 +0545

    Change export syntax from default to module.exports

--- api/instagram.js
+++ api/instagram.js
@@ -4,7 +4,7 @@
 // no CORS proxy needed, since CORS only applies to browser-to-server
 // requests, not server-to-server ones.
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
